66a4912a46900ff7382453384165afbde7a73f66
Craig Wm. Versek <cversek@gmail.com>
2017-02-07 03:03:55 -0500

    added templates to filesystem loading

--- load_scripts.sh
+++ load_scripts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 ampy -p /dev/ttyUSB0 -b 115200 put SECRET_CONFIG.json
+ampy -p /dev/ttyUSB0 -b 115200 mkdir templates
+ampy -p /dev/ttyUSB0 -b 115200 put ./templates/404.html /templates/404.html
+ampy -p /dev/ttyUSB0 -b 115200 put ./templates/pins.html /templates/pins.html
+ampy -p /dev/ttyUSB0 -b 115200 put ./templates/pins.js /templates/pins.js
+ampy -p /dev/ttyUSB0 -b 115200 put ./templates/pins_table_row.html /templates/pins_table_row.html
 ampy -p /dev/ttyUSB0 -b 115200 put network_setup.py
 ampy -p /dev/ttyUSB0 -b 115200 put time_manager.py
 ampy -p /dev/ttyUSB0 -b 115200 put dump_logs.py
