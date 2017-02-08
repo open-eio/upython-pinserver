e379141d1e660360ef28704f71d737e5cb297000
Craig Versek <cversek@gmail.com>
2017-02-08 15:09:31 -0500

    got rid of superfluous absolute page address

--- templates/pins.js
+++ templates/pins.js
@@ -9,7 +9,7 @@ document.body.addEventListener("click", function(event) {
 function postToggle (btn_id) {
   var form = document.createElement('form');
   form.setAttribute('method', 'post');
-  form.setAttribute('action', 'http://{{ server_base_url }}?btn_id='+btn_id);
+  form.setAttribute('action', '/?btn_id='+btn_id);
   form.style.display = 'hidden';
   document.body.appendChild(form)
   form.submit();
