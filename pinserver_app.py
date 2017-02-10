1321f6ffa18b6e0975f86b56df9f858faf2b0a77
Craig Wm. Versek <cversek@gmail.com>
2017-02-10 02:27:36 -0500

    match CircuitPython's machine.Pin interface; better debuging output

--- pinserver_app.py
+++ pinserver_app.py
@@ -1,15 +1,10 @@
 ################################################################################
 # STANDARD LIB IMPORTS
-import os, time
+import sys, os, time
 try:
     from collections import OrderedDict
 except ImportError:
     from ucollections import OrderedDict #micropython specific
-    
-try:
-    import machine
-except ImportError:
-    import mock_machine as machine #a substitute for PC testing
     
 try:
     import json
@@ -23,18 +18,21 @@ from pawpaw.web_app import Router
 
 #-------------------------------------------------------------------------------
 # LOCAL IMPORTS
-import network_setup
-
 DEBUG = True
 
 if DEBUG:
-    print("INSIDE MODULE name='%s' " % ('pinserver_app'))
+    print("INSIDE MODULE name='%s' " % ('pinserver_app',))
     try:
-        from micropython import mem_info
+        from micropython import mem_info #micropython specific
         mem_info()
     except ImportError:
         pass
 
+try:
+    import machine #micropython specific
+except ImportError:
+    import mock_machine as machine #a substitute for PC testing
+    
 ################################################################################
 # CONFIGURATION
 #-------------------------------------------------------------------------------
@@ -50,28 +48,49 @@ if CONFIG_FILENAME in os.listdir():
 app_cfg = config.get('pinserver_app', {})
 DEBUG   = app_cfg.get('debug', 0)
 
-SERVER_ADDR = app_cfg.get('server_addr','0.0.0.0')  #default to localhost on PC
-SERVER_PORT = app_cfg.get('server_port',9999)
+SERVER_ADDR = app_cfg.get('server_addr')
+SERVER_PORT = app_cfg.get('server_port')
 
-# ------------------------------------------------------------------------------
-# Network/Services setup
-sta_if, ap_if = network_setup.do_connect(**config['network_setup'])
-
+################################################################################
+# PLATFORM SPECIFIC SETUP
+#-------------------------------------------------------------------------------
+PLATFORM = sys.platform
 if DEBUG:
-    print("INSIDE MODULE name='%s': after network_setup.do_connect " % ('pinserver_app'))
-    print("\tSERVER_ADDR = %s" % SERVER_ADDR)
-    print("\tSERVER_PORT = %s" % SERVER_PORT)
-    try:
-        from micropython import mem_info
-        mem_info()
-    except ImportError:
-        pass
-
+    print("DETECTED PLATFORM: %s" % PLATFORM)
+# ------------------------------------------------------------------------------
+# ESP8266
+if PLATFORM == 'esp8266':
+    if SERVER_ADDR is None:
+        SERVER_ADDR = "192.168.4.1" #default to AP interface
+        if DEBUG:
+            print("DEFAULTING SERVER_ADDR to '%s'" % SERVER_ADDR)
+    if SERVER_PORT is None:
+        SERVER_PORT = 80            #default HTTP port
+        if DEBUG:
+            print("DEFAULTING SERVER_PORT to '%s'" % SERVER_PORT)
+    # Network/Services setup
+    import network_setup
+    if DEBUG:
+        print("RUNNING network_setup.do_connect:")
+    sta_if, ap_if = network_setup.do_connect(**config['network_setup'])
+    time.sleep(1.0)
+   
+# ------------------------------------------------------------------------------
+# DEFAULT PLATFORM
+else:
+    if SERVER_ADDR is None:
+        SERVER_ADDR = "0.0.0.0" #default to localhost
+        if DEBUG:
+            print("DEFAULTING SERVER_ADDR to '%s'" % SERVER_ADDR)
+    if SERVER_PORT is None:
+        SERVER_PORT = 8080      #alternate HTTP port
+        if DEBUG:
+            print("DEFAULTING SERVER_PORT to '%s'" % SERVER_PORT)
 ################################################################################
 # GLOBALS
 #-------------------------------------------------------------------------------
 PIN_NUMBERS = (0, 2, 4, 5, 12, 13, 14, 15)
-PINS = OrderedDict((i,machine.Pin(i, machine.Pin.IN)) for i in PIN_NUMBERS)
+PINS = OrderedDict((i,machine.Pin(i, machine.Pin.OUT)) for i in PIN_NUMBERS)
 
 ################################################################################
 # APPLICATION CODE
@@ -98,18 +117,18 @@ class PinServer(WebApp):
         
         if context.request.method == 'POST':
             if DEBUG:
-                print("HANDLING POST REQUEST: args = %r" % context.request.args)
+                print("HANDLING POST REQUEST: req = %r" % context.request)
             #get the button id from the params and pull out the corresponding pin object
             btn_id = context.request.args['btn_id']
             pin_id = int(btn_id[3:])#pattern is "btn\d\d"
             pin = PINS[pin_id]
-            pin.value = not pin.value() #invert the pin state
+            pin.value(not pin.value()) #invert the pin state
             comment = "Toggled pin %d" % pin_id
         
         #we make table content a generator that produces one row per iteration
         def gen_table_content(pins):
             for pin_num, pin in pins.items():
-                ptr_tmp.format(pin_id = str(pin),
+                ptr_tmp.format(pin_id = pin_num,
                                pin_value = 'HIGH' if pin.value() else 'LOW',
                               )
                 for line in ptr_tmp.render():
